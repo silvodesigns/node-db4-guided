4baf3f5a93d02a9b26400f83f2ad6d04ae0451c1
silvodesigns <kevsilvo@gmail.com>
2020-02-07 23:59:28 -0500

    completed follow along

---
+++ data/migrations/20200207005924_create-tables.js
@@ -0,0 +1,46 @@
+
+exports.up = function(knex) {
+    return knex.schema.createTable('zoos', tbl => {
+        tbl.increments();
+        tbl.string('zoo_name', 128).notNullable();
+        tbl.string('address', 128).notNullable().unique();
+    })
+    .createTable('species', tbl => {
+        tbl.increments();
+        tbl.string('species_name',128).notNullable().unique();
+    })
+    .createTable('animals', tbl => {
+        tbl.increments();
+        tbl.string('animal_name',128).notNullable();
+        tbl.integer('species_id')
+            .unsigned()
+            .notNullable()
+            .references('id')
+            .inTable('species')
+    })
+    .createTable('zoo_animals', tbl => {
+        tbl.integer('zoo_id')
+            .unsigned()
+            .notNullable()
+            .references('id')
+            .inTable('zoos')
+
+         tbl.integer('animal_id')
+            .unsigned()
+            .notNullable()
+            .references('id')
+            .inTable('animals')
+        tbl.primary(['zoo_id', 'animal_id']);
+    })
+  
+};
+
+exports.down = function(knex) {
+    //drop table in the reverse order that you create them
+    return knex.schema
+    .dropTableIfExists('zoo_animals')
+    .dropTableIfExists('animals')
+    .dropTableIfExists('species')
+    .dropTableIfExists('zoo')
+  
+};
